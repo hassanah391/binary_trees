2de1c53e0f127a0cb44f5b8ad0ad759732e62cfc
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-03 14:54:13 +0200

    Ensure binary_tree_node returned a valid pointer before proceeding

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,14 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *new_node;
 	binary_tree_t *old_node;
 
 	new_node = binary_tree_node(parent, value);
-
-	if (parent == NULL)
+	if (new_node == NULL) /* Check allocation success */
 		return (NULL);
 
 	if (parent->left == NULL)
