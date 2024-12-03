4ce7137ca5e7c31d7f4344b8e62a96ab917bbd95
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-03 14:58:40 +0200

    follow betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,11 +9,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
 	binary_tree_t *new_node;
 	binary_tree_t *old_node;
+
+	if (parent == NULL)
+		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL) /* Check allocation success */
