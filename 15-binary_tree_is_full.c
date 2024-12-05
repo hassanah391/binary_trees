5409185ed8a3ccb759b35719f8818edebc04c532
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-05 07:04:47 +0200

    Made the code more readable

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,9 +2,12 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left != NULL && tree->right == NULL)
-	    || (tree->left == NULL && tree->right != NULL))
+	if (tree == NULL)
 		return (0);
+
+	if ((tree->left != NULL && tree->right == NULL) ||
+        (tree->left == NULL && tree->right != NULL))
+        return (0);
 
 	if (tree->left == NULL && tree->right ==NULL)
 		return (1);
